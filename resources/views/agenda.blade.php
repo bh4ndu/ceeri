019f983b5a30da18f2b5e6b471fa872dd5ac7cb3
jottahs <jottahs136@gmail.com>
2023-09-01 12:09:01 +0200

    falta sistema

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -1,80 +1,69 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>CEERI</title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CEERI</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="agenda.css">
 </head>
 <body>
-    <!-- LOGO -->
-    <div class="fixed-top" style="background:skyblue">
+    <!-- Encabezado -->
+    <div class="fixed-top bg-primary">
         <div class="container">
-            <!-- LOGO -->
-            <div class="row align-items-start">
+            <div class="row align-items-center">
                 <div class="col-2">
-                    <img class="logo" src="img/post/1.jpg" alt="Logo">
+                    <img class="logo img-fluid" src="img/post/1.jpg" alt="Logo">
                 </div>
                 <div class="col-3">
-                    <h3 class="company-name">CENTRO ESPECIALIZADO <br> EN REHABILITACIÓN INTEGRAL</h3>
+                    <h3 class="company-name">CENTRO ESPECIALIZADO<br>EN REHABILITACIÓN INTEGRAL</h3>
                 </div>
-                <div class="col-2 reservation-btn">
-                    <button class="btn btn-primary"><a href="/agenda" class="reserva-btn">citas agendadas</a></button>
+                <div class="col-2">
+                    <button class="btn btn-light"><a href="/agenda" class="reserva-btn text-primary">Citas Agendadas</a></button>
                 </div>
-                <div class="col-3 reservation-btn">
-                    <button class="btn btn-primary"><a href="/historial" class="reserva-btn">historial de citas</a></button>                                      
+                <div class="col-3">
+                    <button class="btn btn-light"><a href="/historial" class="reserva-btn text-primary">Historial de Citas</a></button>
                 </div>
             </div>
-       </div>
+        </div>
     </div>
-    <br> 
-    
-        <section class="container">
-            <div class="row align-items-start">
-                @foreach ($citas as $cita)
-                    <div class="tarjeta col-md-5">
-                        <h3>tipo de documento: {{$cita->tipo_documento}}</h3>
-                        <h3>numero de documento: {{$cita->numero_documento}}</h3>
-                        <h3>nombres: {{$cita->nombres}}</h3>
-                        <h3>apellidos: {{$cita->apellidos}}</h3>
-                        <h3>telefono: {{$cita->telefono}}</h3>
-                        <h3>especialidad: {{$cita->especialidad}}</h3>
-                        <h3>genero: {{$cita->genero}}</h3>
-                        <h3>fecha y hora: {{$cita->fecha_hora}}</h3>
-                       
-                        <form id="citaForm">
-                            <select name="estado[]" class="estadoSelect">
-                                <option value="cumplido">cumplido</option>
-                                <option value="ausente">ausente</option>
-                                <option value="cancelada">cancelada</option>
-                            </select>
-                        </form>
-                        <div class="reservation-btn">
-                            <button class="btn btn-primary"><a class="reserva-btn">enviar al historial</a></button>                                      
+    <br>
+
+    <!-- Sección de citas -->
+    <section class="container">
+        <div class="row">
+            @foreach ($citas as $cita)
+                <div class="col-md-5">
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <h3 class="card-title">Tipo de documento: {{$cita->tipo_documento}}</h3>
+                            <p class="card-text">Número de documento: {{$cita->numero_documento}}</p>
+                            <p class="card-text">Nombres: {{$cita->nombres}}</p>
+                            <p class="card-text">Apellidos: {{$cita->apellidos}}</p>
+                            <p class="card-text">Teléfono: {{$cita->telefono}}</p>
+                            <p class="card-text">Especialidad: {{$cita->especialidad}}</p>
+                            <p class="card-text">Género: {{$cita->genero}}</p>
+                            <p class="card-text">Fecha y hora: {{$cita->fecha_hora}}</p>
+                            <form class="citaForm">
+                                <select name="estado[]" class="form-control">
+                                    <option value="cumplido">Cumplido</option>
+                                    <option value="ausente">Ausente</option>
+                                    <option value="cancelada">Cancelada</option>
+                                </select>
+                            </form>
+                            <div class="mt-2">
+                                <button class="btn btn-primary"><a href="#" class="text-light">Enviar al Historial</a></button>
+                            </div>
                         </div>
-                        <br>
                     </div>
-                @endforeach 
-            </div>         
-        </section>
-<script>
-    // Obtener todos los elementos select con la clase "estadoSelect"
-    const estadoSelects = document.querySelectorAll('.estadoSelect');
+                </div>
+            @endforeach
+        </div>
+    </section>
 
-    // Iterar sobre todos los selectores y aplicar el script a cada uno
-    estadoSelects.forEach((estadoSelect, index) => {
-        // Generar una clave única para cada selector basada en su índice
-        const storageKey = `selectedValue_${index}`;
-
-        const storedValue = localStorage.getItem(storageKey);
-
-        if (storedValue) {
-            estadoSelect.value = storedValue;
-        }
-
-        estadoSelect.addEventListener('change', function() {
-            const selectedValue = estadoSelect.value;
-            localStorage.setItem(storageKey, selectedValue);
-        });
-    });
-</script>
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
